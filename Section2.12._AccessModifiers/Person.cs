bbf203bde1ad6afbf8c429db31f80eae98021445
Abby Howe <howe.abigail@gmail.com>
2020-10-15 22:21:53 -0500

    * Person.cs: * Program.cs: * Section2.12._AccessModifiers.csproj:
    * CSharp_Intermediate.sln: Section 2.12 Access Modifiers Exercises
      Completed.

---
+++ Section2.12._AccessModifiers/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Section2._12._AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+
+
+}
---
+++ Section2.12._AccessModifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Section2._12._AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Section2._12._AccessModifiers");
+
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 01, 01));
+            Console.WriteLine(person.GetBirthdate());
+            
+        }
+
+    }
+
+    
+}
