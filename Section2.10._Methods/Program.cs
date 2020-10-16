177780465aae598bd67836bd6372b372154fc8e0
Abby Howe <howe.abigail@gmail.com>
2020-10-15 19:52:30 -0500

    * Calculator.cs: Methods Practice Files Completed
    * Point.cs:
    * Program.cs:

--- Section2.10._Methods/Program.cs
+++ Section2.10._Methods/Program.cs
@@ -6,6 +6,39 @@ namespace Section2._10._Methods
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+               var num = int.Parse("abc");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Conversion Failed (in the try catch block)");
+            }
+            
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed (TryParse).");
+            }
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5}));
+        }
+
+        static void UsePoints()
+        {
             Console.WriteLine("Section2._10._Methods");
 
             //Goals for Learning:
@@ -14,6 +47,21 @@ namespace Section2._10._Methods
              * Ref Modifier //Code Smells & Not Useful
              * Out Modifier //Code Smells & Not Useful
              */
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occured");
+
+            }
         }
     }
 }
