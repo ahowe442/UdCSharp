25456b683c836e968da05f3c3fba5429fd564702
cohowe <howe.abigail@gmail.com>
2020-10-16 15:34:16 -0500

    * Person.cs: * Program.cs: * Section2.13._Properties.csproj:
    * CSharp_Intermediate.sln: Section 2.13 Completed

---
+++ Section2.13._Properties/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Section2._13._Properties
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ Section2.13._Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Section2._13._Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var person = new Person();
+            person.Birthdate = new DateTime(1982, 1, 1);
+            Console.WriteLine(person.Age); 
+        }
+    }
+}
